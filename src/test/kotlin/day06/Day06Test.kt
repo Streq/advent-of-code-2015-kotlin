fb6844fc9d2f518984166423a84b1a7c184b4f1d
streq <strequena@gmail.com>
2025-03-18 17:04:01 -0300

    day 6

--- src/test/kotlin/day06/Day06Test.kt
+++ src/test/kotlin/day06/Day06Test.kt
@@ -17,7 +17,7 @@ class Day06Test {
     @Test
     fun testPart2() {
         assertEquals(1, Day06.Part2.solve("turn on 0,0 through 0,0"))
-        assertEquals(2000000, Day06.Part2.solve("toggle 0,0 through 999,999"))
+        assertEquals(2000_000, Day06.Part2.solve("toggle 0,0 through 999,999"))
     }
 }
 
