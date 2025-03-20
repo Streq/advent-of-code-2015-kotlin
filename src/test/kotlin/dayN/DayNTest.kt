e782aff4dea88e4a414387103b55c5d63e8dae72
streq <strequena@gmail.com>
2025-03-19 22:46:27 -0300

    DayNTest

--- src/test/kotlin/dayN/DayNTest.kt
+++ src/test/kotlin/dayN/DayNTest.kt
@@ -1,22 +1,17 @@
 package dayN
 
 import org.junit.jupiter.api.Assertions.*
-import util.exampleInputPath
 import kotlin.test.Test
-
-object Input {
-    val EXAMPLE: String = exampleInputPath(object {}.javaClass)
-}
 
 class DayNTest {
     @Test
     fun testPart1() {
-        assertEquals(0, DayN.Part1.solveFromFile(Input.EXAMPLE))
+        assertEquals(0, DayN.Part1.solveFromRawFile(""""""))
     }
 
     @Test
     fun testPart2() {
-        assertEquals(0, DayN.Part2.solveFromFile(Input.EXAMPLE))
+        assertEquals(0, DayN.Part2.solveFromFile(""""""))
     }
 }
 
